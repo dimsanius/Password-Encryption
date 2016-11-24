8914f5533095e1115062a3a85d330744c7552ad7
dimsanius <dmitrij.santarovic@gmail.com>
2016-11-24 00:03:46 +0000

    First commit
    Uploading files

---
+++ Password encyption(discontinued)/Password encyption/Root.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Password_encyption
+{
+    partial class Root
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(375, 84);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose option:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(548, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addItemToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // addItemToolStripMenuItem
+            // 
+            this.addItemToolStripMenuItem.Name = "addItemToolStripMenuItem";
+            this.addItemToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.addItemToolStripMenuItem.Text = "Add item";
+            this.addItemToolStripMenuItem.Click += new System.EventHandler(this.addItemToolStripMenuItem_Click);
+            // 
+            // Root
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Root";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Root_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addItemToolStripMenuItem;
+    }
+}
+
---
+++ Password encyption(discontinued)/Password encyption/Root.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Password_encyption
+{
+    public partial class Root : Form
+    {
+        private static string ValueToPass;
+        public static string PassedValueManagement
+        {
+            get { return ValueToPass; }
+            set { ValueToPass = value;  }
+        }
+
+        
+        public Root()
+        {
+            InitializeComponent();
+            listBox1.MouseDoubleClick += new MouseEventHandler(listBox1_MouseDoubleClick);
+            
+        }
+
+
+        public void addItemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            Form createNewOption = new AddItem();
+            createNewOption.ShowDialog();
+
+            listBox1.Items.Add(PassedValueManagement);
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + @"/Options/"))
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"/Options");
+            StreamWriter streamWriter = new StreamWriter(Directory.GetCurrentDirectory() + "/Options/" + PassedValueManagement);
+            streamWriter.Close();
+        }
+
+        private void Root_Load(object sender, EventArgs e)
+        {
+            foreach (string s in Directory.GetFiles(String.Format(Directory.GetCurrentDirectory() + @"/Options")).Select(Path.GetFileName))
+                listBox1.Items.Add(s);
+
+
+        }
+
+
+        
+
+        void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+        MessageBox.Show(String.Format("{0}", listBox1.SelectedIndex.ToString()));
+         
+     }
+
+    }
+}
